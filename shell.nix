a72a2ca0299ada15a96d9ffb37fd1bdbf9db280e
Kreyren <kreyren@fsfe.org>
2021-10-18 16:51:56 +0200

    shell.nix: Initial commit
    This file profiles instructions for nix <https://en.wikipedia.org/wiki/Nix_package_manager> to standardize a reproducible environment for dependencies needed to work with the source code namely stable rustlang backend from mozilla overlay

---
+++ shell.nix
@@ -0,0 +1,13 @@
+# NixOS shell configuration to bootstrap the required dependencies
+
+let
+	moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+	nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+in
+	with nixpkgs;
+	stdenv.mkDerivation {
+		name = "moz_overlay_shell";
+		buildInputs = [
+			nixpkgs.latest.rustChannels.stable.rust
+		];
+	}
